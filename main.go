364f731bc3c3a38ab13f756cf62d598a227102c8
George Yameen <gmyameen@gmail.com>
2019-02-21 19:24:27 -0800

    lol

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
-package GoLang
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello everyone")
+	foo()
+	fmt.Println("something more")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
+
+//control flow"
+// 1 sequence
+// 2 loop
+// 3 conditional
