26d85fb960d7e7cf6c3a82ce03f84ca499cbfb53
George Yameen <gmyameen@gmail.com>
2019-02-25 14:55:58 -0800

    you're doing great

--- exercise2/main.go
+++ exercise2/main.go
@@ -2,13 +2,12 @@ package main
 
 import "fmt"
 
+var x int
+var y string
+var z bool
+
 func main() {
-	x := 42
 	fmt.Println(x)
-	x = 99
-	fmt.Println(x)
-	y := 100 + 70
 	fmt.Println(y)
-	z := "Bond, James"
 	fmt.Println(z)
 }
